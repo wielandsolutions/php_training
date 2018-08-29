3a602256fea5dcc5c1c4d35262030e7dc758ecfe
unknown <manjeet.kumar@wielandsolutions.com>
2018-08-29 17:07:20 +0530

    echoing dom element

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype <!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+</body>
+</html>>
