d519547b4550dbd5f2b8232e612d470fb90cac20
[pushpraj singh] <[pushparaj@wielandsolutions.com]>
2018-08-29 17:57:59 +0530

    add echo  = 10

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x = 5985;
-var_dump($x);
+$x = 5; // global scope
+$y = 10;
+function myTest() {
+    // using x inside this function will generate an error
+    echo "<p>Variable x inside function is: $x</p>";
+} 
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
 ?>
 
 </body>
